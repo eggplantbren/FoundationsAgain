b85be69daceb40fa1eee42810a4165d4552a388d
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-03-11 11:09:21 +1300

    Normalise

--- Main.hs
+++ Main.hs
@@ -7,6 +7,6 @@ main :: IO ()
 main = do
   -- A probability distribution
   let p = fromMaybe (error "Failure in makeUniform")
-                    (makeUniform 3)
+                    (makeFromList [1.0, 2.0, 3.0])
   print p
 
--- ProbabilityDistribution.hs
+++ ProbabilityDistribution.hs
@@ -1,14 +1,27 @@
 -- Module name
 module ProbabilityDistribution
-       (makeUniform) where
+       (makeFromList, makeUniform) where
 
 -- Imports
-import Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed as U
 
 -- A type to represent a discrete probability distribution
 -- over N atoms.
 data ProbabilityDistribution = ProbabilityDistribution (U.Vector Double)
   deriving (Eq, Show)
+
+-- Normalise a list
+normaliseList :: [Double] -> [Double]
+normaliseList xs = map (/ tot) xs where
+  tot = sum xs
+
+-- Smart constructor from a list of nonnegative doubles
+makeFromList :: [Double] -> Maybe ProbabilityDistribution
+makeFromList [] = Nothing
+makeFromList ps
+  | any (< 0.0) ps = Nothing
+  | otherwise      = Just (ProbabilityDistribution ps') where
+                       ps' = U.fromList (normaliseList ps)
 
 -- Smart constructor of uniform distributions
 makeUniform :: Int -> Maybe ProbabilityDistribution
@@ -16,4 +29,5 @@ makeUniform n
   | n <= 0    = Nothing
   | otherwise = Just (ProbabilityDistribution vec) where
                   vec = U.replicate n (1.0 / fromIntegral n)
+
 
