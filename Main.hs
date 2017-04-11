0a2f1270f04c4fe3e84783b5620be254dabafa51
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-11 15:52:44 +1200

    Gutted it

--- Main.hs
+++ Main.hs
@@ -1,25 +1,9 @@
 -- Imports
-import Control.Monad
-import Prelude hiding (and, or)
-import Statement
 
 -- Main action
 main :: IO ()
 main = do
-  -- Create two statements
-  let s1 = fromString "011"
-  let s2 = fromString "101"
-  print s1
-  print s2
-
-  -- Logical or
-  let result = do
-                 temp <- liftM2 or s1 s2
-                 temp
-  print result
-
-  putStrLn ""
-  print $ allStatements 4
+  putStrLn "Hello."
 
   return ()
 
