256f6ad9ba9de9f00ab7ca2bf724dc4ec7a04a37
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-15 22:37:37 +1200

    logical and, or

--- Measure.hs
+++ Measure.hs
@@ -1,3 +1,4 @@
 module Measure where
 
+import Statement
 
--- Statement.hs
+++ Statement.hs
@@ -5,7 +5,8 @@ module Statement where
 -- Imports
 import Data.List (subsequences)
 import qualified Data.Set as S
-
+import qualified Data.Vector as V
+import Prelude hiding (and, or)
 
 -- These two things (an id and the number of atoms)
 -- are the defining features of a hypothesis space
@@ -33,15 +34,34 @@ render (Statement hs ias) =
   in
     hypothesisSpaceID hs ++ mconcat (map show bits)
 
+
 -- All statements in a hypothesis space with the
 -- given label and number of atoms
-allStatements :: HypothesisSpace -> [Statement]
+allStatements :: HypothesisSpace -> V.Vector Statement
 allStatements hypothesisSpace@(HypothesisSpace _ numAtoms) =
   let
     ss   = subsequences [0..(numAtoms-1)] :: [[Int]]
     incl = map S.fromList ss              :: [S.Set Int]
   in
-    map (Statement hypothesisSpace) incl
+    V.fromList $ map (Statement hypothesisSpace) incl
+
+
+
+-- Define logical and
+and :: Statement -> Statement -> Statement
+and (Statement hs1 ias1) (Statement hs2 ias2)
+  | hs1 /= hs2 = error "Not in same space."
+  | otherwise  = Statement hs ias where
+                   hs  = hs1
+                   ias = S.intersection ias1 ias2
+
+-- Define logical or
+or :: Statement -> Statement -> Statement
+or (Statement hs1 ias1) (Statement hs2 ias2)
+  | hs1 /= hs2 = error "Not in same space."
+  | otherwise  = Statement hs ias where
+                   hs  = hs1
+                   ias = S.union ias1 ias2
 
 -- A demo that can be executed
 demo :: IO ()
@@ -50,8 +70,10 @@ demo = do
   -- Create a whole Boolean lattice of statements!
   let hs = HypothesisSpace "x" 3
   let ss = allStatements hs
-  let strings = map render ss
-  print strings
 
-
+  let x = ss V.! 3
+  let y = ss V.! 4
+  putStrLn $ render x
+  putStrLn $ render y
+  putStrLn $ render (x `or` y)
 
